824d90294d9f27193869e351538f28f0815e6194
tareqzoubii <tareqzoubi4@gmail.com>
2022-09-05 21:56:46 -0700

    V2

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ server.use(cors());
 require('dotenv').config();
 const mongoose = require('mongoose') // step 0 (import the mongoose!)
 
+server.use(express.json()); // access the req.body!!
+
 const BookModel = require('./Books.js')
 const PORT = process.env.PORT || 3888;
 
@@ -17,6 +19,8 @@ mongoose.connect('mongodb://localhost:27017/booksApp', {useNewUrlParser: true, u
 //Routes Section 
 server.get('/', homeHandler);
 server.get('/getBooks', booksHandler);
+server.post('/addNewBook', addNewBookHandler);
+server.delete('/deleteBooks/:id', deleteBooksHandler);
 
 
 //Function Section
@@ -36,11 +40,50 @@ function booksHandler(req, res){
         else
         {
             console.log(result);
-            res.send(result)
+            res.send(result);
         }
     })
+}
 
-
+// http:localhost:3888/addNewBook
+async function addNewBookHandler(req, res){
+    //console.log(req.body)
+    const {title, description,status} = req.body;
+    await BookModel.create({
+        title: title,
+        description: description,
+        status: status
+    })
+    BookModel.find({}, (err, result) => {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            //console.log(result);
+            res.send(result);
+        }
+    })
+}
+// http://localhost:3888/deleteBooks:id
+function deleteBooksHandler(req, res){
+    const bookID = req.params.id;
+    console.log(bookID);
+    BookModel.deleteOne({_id:bookID},(err, result) => {
+        //console.log(result);
+        BookModel.find({}, (err, result) => {
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                //console.log(result);
+                res.send(result);
+            }
+        })
+    })
 }
 server.listen(PORT, () => {
     console.log(`Hello, this is PORT --> ${PORT}`);
